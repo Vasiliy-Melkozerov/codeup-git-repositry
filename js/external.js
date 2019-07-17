c9f7caf033bfc496b81d2f609ba778f0b54838dc
Vasiliy Melkozerov <vasiliy.melkozerov@gmail.com>
2019-07-17 18:21:03 -0500

    refactored and double checked

--- js/external.js
+++ js/external.js
@@ -3,31 +3,32 @@
 console.log("Hello from external JavaScript");
 
 alert('Welcome to my Website!');
-var userColor = prompt("What is your favorite color?");
+let userColor = prompt("What is your favorite color?");
 alert("What a coincidence, " + userColor + " is my favorite color too!");
 
-var pacificRimRental = Number(prompt("How many days are you renting Pacific Rim?"));
-var avengersUltronRental = Number(prompt("How many days are you renting Avengers: Age of Ultron?"));
-var fastAndFurious = Number(prompt("How many days are you renting Fast and Furious 12?"));
+let theLittleMermaid = Number(prompt("How many days are you renting the Little Mermaid?"));
+let brotherBear = Number(prompt("How many days are you renting Brother Bear?"));
+let hercules = Number(prompt("How many days are you renting Hercules?"));
 
-var total = (pacificRimRental + avengersUltronRental + fastAndFurious) * 3;
+let total = (theLittleMermaid + brotherBear + hercules) * 3;
 
 alert("You owe the Hollywood Video: $" + total.toFixed(2));
 
-var favorRate = 320;
-var uberRate = 400;
-var writingCode = 200;
+let googleRate = 400;
+let amazonRate = 380;
+let facebookRate = 350;
 
-var favorHours = Number(prompt("How many hours did you deliver for Favor this week?"));
-var uberHours = Number(prompt("How many hours did you drive Uber this week?"));
-var codingHours = Number(prompt("How many hours did you freelance code this week?"));
+let google = Number(prompt("How many hours did you work for Google this week?"));
+let amazon = Number(prompt("How many hours did you work for Amazon this week?"));
+let facebook = Number(prompt("How many hours did you work for Facebook this week?"));
 
-alert("You earned $" + ((favorHours * favorRate) + (uberHours * uberRate) + (codingHours * writingCode)).toFixed(2));
+alert("You earned $" + ((google * googleRate) + (amazon * amazonRate) + (facebook * facebookRate)).toFixed(2));
 
-var classTime = prompt("What time is the class?");
-var noScheduleConflict = !confirm("Does the student have a class at " + classTime);
+let classTime = prompt("What time is the class?");
+let noScheduleConflict = confirm("Is the student available at " + classTime + "?");
 
-var maxClassSize = Number(prompt("What's the maximum class size?"));
-var studentsEnrolled = Number(prompt("How many students are currently enrolled?"));
-console.log(studentsEnrolled < maxClassSize);
+let maxClassSize = Number(prompt("What's the maximum class size?"));
+let studentsEnrolled = Number(prompt("How many students are currently enrolled?"));
+console.log(studentsEnrolled <= maxClassSize);
+
 alert("Student can enroll in class: " + (noScheduleConflict && (studentsEnrolled < maxClassSize)));
