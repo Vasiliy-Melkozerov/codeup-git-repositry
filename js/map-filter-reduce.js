c1c17d3a0f54b405cea480dc5cf398f62d6b8f73
Vasiliy Melkozerov <vasiliy.melkozerov@gmail.com>
2019-07-17 15:04:42 -0500

    bonus finished

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -43,10 +43,13 @@ console.log(language);
 const emails = users.map(current => current.email);
 console.log(emails);
 // TODO:Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
-const years = users.reduce((accumulater, current) => {
-    return accumulater + current.yearsOfExperience;
-}, 0);
-console.log(years);
+// const years = users.reduce((total, users) => {
+//     return total + users.yearsOfExperience;
+// }, 0);
+// console.log(years);
+const totalYears = users.reduce((total,user)=> total + user.yearsOfExperience,0);
+const averageYears = totalYears/ users.length;
+console.log(averageYears);
 // TODO:Use .reduce to get the longest email from the list of users.
 const lgemail = users.reduce((accumulater, current) => {
     const currentEmail = current.email;
@@ -58,7 +61,20 @@ const lgemail = users.reduce((accumulater, current) => {
 },"");
 console.log(lgemail);
 //TODO: Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
-const single = users.reduce((accumulater, current) =>{
-    return accumulater + `${current.name}, `;
+const single = users.reduce(function(sentence, instructor,index,arr){
+    if(index ===(arr.length -1)){
+        return `${sentence}${instructor.name}.`
+    }else{
+        return `${sentence}${instructor.name},`
+    }
 },"You're instructors names are: ");
 console.log(single);
+//TODO:Use .reduce to get the unique list of languages from the list of users.
+const unique = users.reduce(function(prev,current){
+    const langs = current.languages;
+    for(const lang of langs){
+        prev.add(lang);
+    }
+    return prev;
+},new Set);
+console.log(unique);
